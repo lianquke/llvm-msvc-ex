0f90c7ca8f1ed152bee3b10ac6a96b8f11e66de9
invalid <you@example.com>
2024-08-22 21:36:20 +0800

    08/22/2024 21:36:19:795

---
+++ llvm/lib/Obfuscation/AliasAccess.h
@@ -0,0 +1,13 @@
+//
+// Created by root on 8/22/2024.
+//
+
+#ifndef ALIASACCESS_H
+#define ALIASACCESS_H
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+}
+#endif //ALIASACCESS_H
